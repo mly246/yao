c3a5be0f4959f84611a856af0d109accdb3c70d4
wjx36 <wjx24786@qq.com>
2024-12-09 09:42:22 +0800

    wjx V1

---
+++ rs_rscode.m
@@ -0,0 +1,10 @@
+%RS码编码
+functioncode=rs_rscode(msg)
+g=[1003213
+0105514
+0017616];
+g=gf(g,3);
+code=msg*g;
+functionnew_msg=rs_rrscode(in_msg,all_code_msg,all_msg)
+new_msg=rs_interpret(in_msg,all_code_msg,all_msg);
+return;
